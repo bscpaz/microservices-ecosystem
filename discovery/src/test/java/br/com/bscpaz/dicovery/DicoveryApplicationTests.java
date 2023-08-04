7fed9af74d4e8c9f5a8778574621f55d6df59425
Bruno Paz <soujava@gmail.com>
2023-08-04 16:54:03 -0300

    Adding Service Discovery

---
+++ discovery/src/test/java/br/com/bscpaz/dicovery/DicoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bscpaz.dicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
