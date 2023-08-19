44448e4e8fd553ee20163f7ba793b67e4cb8efe6
Bruno Paz <soujava@gmail.com>
2023-08-19 14:10:20 -0300

    Adding new projects

---
+++ jbpm-ms/src/main/java/br/com/bscpaz/jbpmms/JbpmMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bscpaz.jbpmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbpmMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbpmMsApplication.class, args);
+	}
+
+}
---
+++ jbpm-ms/src/test/java/br/com/bscpaz/jbpmms/JbpmMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bscpaz.jbpmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbpmMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
